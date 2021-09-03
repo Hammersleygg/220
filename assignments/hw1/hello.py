82493bb28bacd8f0819795245bdc17b535843765
gavinhammersley <hammersleygg@g.cofc.edu>
2021-09-03 13:54:44 -0400

    completed lab 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,16 @@
+"""
+Name: Gavin Hammersley
+hello.py
+
+Problem: print Hello, World! to the screen
+
+Certification of authenticity
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+
+main()
